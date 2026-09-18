468bc5268e4d523f33beca20102b8bb5ea22c822
Isabelladelibassis <idelibassis@unr.edu>
2026-09-18 16:56:31 -0700

    Book.cpp

--- Book.cpp
+++ Book.cpp
@@ -1,17 +1,20 @@
-#include "book.h"
+#include "Book.h"
 
-class Book {
-    Book::Book(int i, const std::string& n, std::string a, int p):Resource(i, n) {
+    Book::Book(int i, const string& n, string a, int p):Resource(i, n) {
         author = a;
         pages = p;
     }
 
     void Book::display() const {
-
+        cout << "Book" << endl;
+        cout << endl << "ID: " << id << endl;
+        cout << "Name: " << name << endl;
+        cout << "Author: " << author << endl;
+        cout << "Pages: " << pages << endl;
+        cout << "Cost: $" << usageCost() << endl;
     }
     double Book::usageCost() const {
         double cost;
         cost = pages * 0.01;
         return cost;
     }
-}
